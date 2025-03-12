283bcba9aa8083b302fd54d26f885dcea3c49449
Ben <tux-o-matic@users.noreply.github.com>
2025-03-12 10:18:18 +0100

    Added type hints

--- source/asgi.py
+++ source/asgi.py
@@ -1,28 +1,29 @@
 """Quart ASGI web app."""
 
 import asyncio
+from typing import List
 from hypercorn.config import Config
 from hypercorn.asyncio import serve
 from opentelemetry.instrumentation.asgi import OpenTelemetryMiddleware
 from quart import Quart
 
-config = Config()
-config.accesslog = '-'
-config.bind = ["0.0.0.0:8080"]
-config.errorlog = '-'
+config: Config = Config()
+config.accesslog: str = '-'
+config.bind: List[str] = ["0.0.0.0:8080"]
+config.errorlog: str = '-'
 
-app = Quart(__name__)
+app: Quart = Quart(__name__)
 app.asgi_app = OpenTelemetryMiddleware(app.asgi_app)
 
 
 @app.route('/')
-async def index():
+async def index() -> str:
     """Index"""
     return 'Hello World'
 
 
 @app.route('/health')
-async def health():
+async def health() -> tuple:
     """Heathcheck endpoint"""
     return "OK", 200
 
